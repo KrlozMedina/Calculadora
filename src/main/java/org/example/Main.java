540234121a957396aaedd49e6188f2314a874376
KrlozMedina <charli1216@hotmail.com>
2023-03-04 10:05:51 -0500

    "[POO] Actualization the calculator with POO"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,10 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Hello world!");
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                JFrame frame = new Calculadora();
-                frame.setSize(300, 300);
-                frame.setVisible(true);
-            }
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new Calculator();
+            frame.setSize(400, 200);
+            frame.setVisible(true);
         });
     }
 }
